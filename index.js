d7aad4f434dd3059cf046ca608bfc2b611d6745c
njiji <noelathandi123@gmail.com>
2024-09-30 14:25:02 +0300

    working with functions

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(Location) {
+    const hqLocation =42;
+    return Math.abs(Location-hqLocation);
+   
+    }
+    
+function distanceFromHqInFeet(location) {
+    const hqLocation=42;
+    const distanceInBlocks = Math.abs(location-hqLocation);
+    return distanceInBlocks*264;
+
+}
+
+function distanceTravelledInFeet(start, destination) {
+    const distanceInBlocks = Math.abs(destination-start);
+    return distanceInBlocks*264;
+}
+
+function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+
+    if (distance <= 400) {
+        return 0; 
+    } else if (distance > 400 && distance <= 2000) {
+        const chargeableDistance = distance - 400; 
+        return chargeableDistance * 0.02; 
+    } else if (distance > 2000 && distance <= 2500) {
+        return 25; 
+    } else {
+        return 'cannot travel that far'; 
+    }
+}
